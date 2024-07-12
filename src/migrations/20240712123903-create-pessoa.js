988c5cabd431ce28703f4469e799f08acbb0e716
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-12 10:18:07 -0300

    feat: migrando para o banco de dados

--- src/migrations/20240712123903-create-pessoa.js
+++ src/migrations/20240712123903-create-pessoa.js
@@ -2,39 +2,39 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Pessoas', {
+    await queryInterface.createTable('pessoas', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nome: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       cpf: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       ativo: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       role: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Pessoas');
-  }
+    await queryInterface.dropTable('pessoas');
+  },
 };
