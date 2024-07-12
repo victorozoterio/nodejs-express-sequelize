67a204ac639ca07bedde54935a72e93c5227ecce
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-12 15:45:02 -0300

    feat: criando endpoint para atualizar pessoas

--- src/controllers/Controllers.js
+++ src/controllers/Controllers.js
@@ -11,6 +11,21 @@ class Controller {
       // erro
     }
   }
+
+  async atualiza(req, res) {
+    const { id } = req.params;
+    const dadosAtualizados = req.body;
+    try {
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(
+        dadosAtualizados,
+        Number(id),
+      );
+      if (!foiAtualizado) {
+        return res.status(400).json({ mensagem: 'registro não foi atualizado' });
+      }
+      return res.status(200).json({ mensagem: 'Registro atualizado com sucesso' });
+    } catch (erro) {}
+  }
 }
 
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,16 @@ class Services {
   async pegaTodosOsRegistros() {
     return dataSource[this.model].findAll();
   }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listaDeregistroAtualizado = dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id },
+    });
+    if (listaDeregistroAtualizado[0] === 0) {
+      return false;
+    }
+    return true;
+  }
 }
 
 module.exports = Services;
